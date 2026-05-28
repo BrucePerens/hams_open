d251d87c8aa904eefab98b7c03a70072fcda0007
BrucePerens <bruce@perens.com>
2026-05-28 12:38:53 -0700

    Fixes for Jules.

--- tools/test.py
+++ tools/test.py
@@ -160,6 +160,7 @@ class FailureExtractor:
                 or "Cloudflare URL purge API failed for chunk: API fail" in line
                 or "Cloudflare Tag purge API failed for chunk: API fail" in line
                 or "[BACKUP_WORKER]" in line
+                or "[LOG_ANALYZER]" in line
             ):
                 if self.capturing:
                     self.captured_blocks.append((self.current_context, self.current_block))
