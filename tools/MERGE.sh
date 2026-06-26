0015ec23d21ab4125301ff666f3bfb94f6999634
BrucePerens <bruce@perens.com>
2026-06-26 10:58:40 -0700

    Remove tools/ in favor of hams_shared/tools

--- tools/MERGE.sh
+++
@@ -1,8 +0,0 @@
-# List draft PRs and mark each as ready
-gh pr list --json number,isDraft --jq '.[] | select(.isDraft == true) | .number' | \
-xargs -I {} gh pr ready {}
-
-# List all non-draft PRs and merge them using admin privileges
-gh pr list --json number,isDraft --jq '.[] | select(.isDraft == false) | .number' | \
-xargs -I {} gh pr merge {} --admin --merge --delete-branch
-
