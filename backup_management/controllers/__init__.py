7a6cc2d6fec15ec032973c66b74d181a1664a700
BrucePerens <bruce@perens.com>
2026-07-15 11:48:45 -0600

    Auto: Update license headers and fix lazy kwargs in backup_board controller

--- backup_management/controllers/__init__.py
+++ backup_management/controllers/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright © Bruce Perens K6BP. All Rights Reserved.
-# This software is released under the AGPL-3.0 License.
+# SPDX-License-Identifier: AGPL-3.0-or-later
 from . import board
--- backup_management/controllers/board.py
+++ backup_management/controllers/board.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright © Bruce Perens K6BP. All Rights Reserved.
-# This software is released under the AGPL-3.0 License.
+# SPDX-License-Identifier: AGPL-3.0-or-later
 from odoo import http
 from odoo.http import request
 
 
 class BackupBoard(http.Controller):
     @http.route("/backup/board", type="http", auth="user", website=True)
-    def backup_board(self, **kw):
+    def backup_board(self):
         action = request.env.ref(
             "backup_management.action_backup_board_client", raise_if_not_found=False
         )
