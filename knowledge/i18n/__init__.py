1b7b721fdf68943eaaa1cb4f2c566ac11ee7e8ba
BrucePerens <bruce@perens.com>
2026-07-16 09:15:14 -0600

    [FIX] knowledge/i18n/__init__.py: Update __init__.py

--- knowledge/i18n/__init__.py
+++
@@ -1,6 +0,0 @@
-# This software is distributed under the terms of the Affero General Public License (AGPL-3).
-
-"""
-This is the __init__.py file for the 'i18n' sub-package.
-It makes the 'i18n' directory a Python package to adhere to standard module structure.
-"""
